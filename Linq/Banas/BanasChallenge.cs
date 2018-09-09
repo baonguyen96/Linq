5a8ad71b8f147f7fa897793b5407e54b3015ed04
baonguyen96 <baonguyendrump@gmail.com>
2018-09-08 22:55:46 -0500

    Add Derek Banas' challenges

---
+++ Linq/Banas/BanasChallenge.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq.Banas
+{
+	partial class BanasChallenge
+	{
+		private static readonly Animal[] Animals =
+		{
+			new Animal
+			{
+				Name = "German Shepherd",
+				Height = 25,
+				Weight = 77,
+				AnimalId = 1
+			},
+			new Animal
+			{
+				Name = "Chihuahua",
+				Height = 7,
+				Weight = 4.4,
+				AnimalId = 2
+			},
+			new Animal
+			{
+				Name = "Saint Bernard",
+				Height = 30,
+				Weight = 200,
+				AnimalId = 3
+			},
+			new Animal
+			{
+				Name = "Pug",
+				Height = 12,
+				Weight = 16,
+				AnimalId = 1
+			},
+			new Animal
+			{
+				Name = "Beagle",
+				Height = 15,
+				Weight = 23,
+				AnimalId = 2
+			}
+		};
+
+		private static readonly Owner[] Owners =
+		{
+			new Owner
+			{
+				Name = "Doug Parks",
+				OwnerId = 1
+			},
+			new Owner
+			{
+				Name = "Sally Smith",
+				OwnerId = 2
+			},
+			new Owner
+			{
+				Name = "Paul Brooks",
+				OwnerId = 3
+			}
+		};
+
+
+		public static void Problem1()
+		{
+			var animalNameHeight = Animals.Select(a => new {a.Name, a.Height});
+
+			foreach (var a in animalNameHeight)
+			{
+				Console.WriteLine(a.ToString());
+			}
+
+			Console.WriteLine();
+		}
+
+		public static void Problem2()
+		{
+			var ownerAnimal = from animal in Animals
+			                  join owner in Owners on animal.AnimalId equals owner.OwnerId
+			                  orderby owner.Name
+			                  select new
+			                  {
+				                  owner,
+				                  animal
+			                  };
+
+			foreach (var o in ownerAnimal)
+			{
+				Console.WriteLine($"{o.owner.Name} owns {o.animal.Name}");
+			}
+
+			Console.WriteLine();
+		}
+
+		public static void Problem3()
+		{
+			var ownerAnimalGroup = from owner in Owners
+			                       orderby owner.Name
+			                       select new
+			                       {
+				                       owner = owner,
+				                       animals = from animal in Animals
+				                                 where animal.AnimalId == owner.OwnerId
+				                                 select animal
+			                       };
+
+			foreach (var o in ownerAnimalGroup)
+			{
+				Console.WriteLine($"{o.owner.Name} owns:");
+				foreach (var a in o.animals)
+				{
+					Console.WriteLine($"   {a.Name}");
+				}
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Linq/Banas/BanasChallengeHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Linq.Banas
+{
+	partial class BanasChallenge
+	{
+		private class Owner
+		{
+			public string Name { get; set; }
+			public int OwnerId { get; set; }
+		}
+
+		private class Animal
+		{
+			public string Name { get; set; }
+			public double Weight { get; set; }
+			public double Height { get; set; }
+			public int AnimalId { get; set; }
+
+			public Animal(string name = "No Name",
+				double weight = 0,
+				double height = 0)
+			{
+				Name = name;
+				Weight = weight;
+				Height = height;
+			}
+
+			public override string ToString()
+			{
+				return $"{Name} weighs {Weight}lbs and is {Height} inches tall";
+			}
+		}
+	}
+}
